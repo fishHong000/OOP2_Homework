8ba72e1166a4877d9c397b67ad7ba8536b338c4f
fishHong000 <113582892+fishHong000@users.noreply.github.com>
2022-10-05 21:35:17 +0800

    Update HW2_ticket.java

--- Week3/HW2_ticket.java
+++ Week3/HW2_ticket.java
@@ -26,7 +26,7 @@ class ticketMachine implements Runnable {
     public void run() {
         while (takeTicket(this)) {
         }
-        System.out.println(t.getName() + " 總共賣出" + tTicket + "張票");
+        System.out.println(t.getName() + " totally sold " + tTicket + " tickets");
     }
 
     private synchronized static boolean takeTicket(ticketMachine machine) {
